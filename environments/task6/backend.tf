f8fb7c4dfd3a2338e6f173171b5976523f3ede0d
Pallavibandreddy <pallavibandreddy6@gmail.com>
2026-09-01 17:36:31 +0530

    Add

---
+++ environments/task6/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-fullstack"
+    storage_account_name = "stfullstackcicd2026"
+    container_name       = "tfstate"
+    key                  = "task6.tfstate"
+    use_azuread_auth     = true
+  }
+}
