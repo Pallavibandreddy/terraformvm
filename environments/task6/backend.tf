c569e527042441fb0f0209a485f9f10c22f2a602
Pallavibandreddy <pallavibandreddy6@gmail.com>
2026-09-01 17:46:19 +0530

    Add

--- environments/task6/backend.tf
+++ environments/task6/backend.tf
@@ -4,6 +4,8 @@ terraform {
     storage_account_name = "stfullstackcicd2026"
     container_name       = "tfstate"
     key                  = "task6.tfstate"
-    use_azuread_auth     = true
+
+    use_azuread_auth = true
+    use_oidc         = true
   }
 }
